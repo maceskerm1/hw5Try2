9a43b8fb508f3b0883f52796b0f7709bda9ebb1e
maceskerm1 <maceskerm1@southernct.edu>
2020-10-14 04:50:10 +0000

    Phonebook js better

--- phonebook.js
+++ phonebook.js
@@ -1,36 +1,16 @@
-// a function that will convert an object into a URL query string format
-function queryObjectToString(query) {
-    // get the properties in the query object
-    // for {message:"Hi I am sending an AJAX request", name: "Sahar"};
-    // properties will be ["message", "name" ]
     let properties = Object.keys(query);
-    // create a string int the format "propert=value" for each property in query
-    // arrOfQuesryStrings will be ["message=Hi I am sending an AJAX request", "name=Sahar"]
     let arrOfQuesryStrings = properties.map(prop => prop+"="+query[prop]);
-    //combine the strings in arrOfQuesryStrings wuth &
-    // return value will be "message=Hi I am sending an AJAX request&name=Sahar"
     return(arrOfQuesryStrings.join('&'));
  }
-const urlPrefix="http://35.237.108.9//";
-function sendAjax(){
-        let AJAX = new XMLHttopRequest();
-        AJAX.open (method,url,true)
-        AJAX.onerror = function() {
-                alert("Network error");
-        }};
-const http = require('http');
-const phonehtml = require('./phonebook.html');
-const url = require('url');
-const fs = require('fs');
-const query = require('./query.js');
-console.log("EEE");
 
+document.getElementById("search").addEventListener("click",sendAJAX);
+const urlPrefix="http://35.237.108.9//";
 function sendAjax(){
         let AJAX = new XMLHttopRequest();
      	AJAX.onerror = function() {
                 alert("Network error");
         }
-	AJAX.onload = function(req,res) {
+	AJAX.onload = function() {
 		if (this.status == 200){
 			console.log(this.responseText);
     			res.writeHead(200, {"Content-Type":"text/html"});
@@ -46,12 +26,10 @@ function sendAjax(){
         		}
 		}};
 
-		AJAX.open("GET",urlPrefix+"getNames?letters="+queryObjectToString(display));			
+		AJAX.open("GET",urlPrefix+"Search?name="+queryObjectToString(display));			
 		AJAX.send();
 
 
     		
 };
 
- // attach click event handler to sendAJAX push button
-document.getElementById("search").addEventListener("click",sendAJAX);
